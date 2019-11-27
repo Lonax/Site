36e86d38e3ef85429f4ab2af4da5263642e35ab1
Denis <deniswaren77@gmail.com>
2019-11-27 15:15:00 +0100

    Initial Release

---
+++ index.php
@@ -0,0 +1,62 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<body scroll="no" style="overflow: hidden">
+	<div class="divide"></div>
+	<head>
+	<meta name="keywords" content ="Lonax">
+	<meta name="author" content ="Lonax"
+	<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	   <link rel="icon" href="Favicon.ico">
+     <link type="text/css" rel="stylesheet" href="./files/materialize.css" media="screen,projection">
+    <link rel="stylesheet" href="./files/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="./files/style.css">
+	<script type="text/javascript" src="./files/jquery.js"></script>
+	<link rel="stylesheet" type="text/css" href="./files/star-animation.css">
+	
+	
+
+	
+    <title>Lonax</title>
+    <script>
+			///// magic title changer big wooshies /////
+			(function titleScroller(text) {
+			document.title = text;
+			setTimeout(function() {
+			titleScroller(text.substr(1) + text.substr(0, 1,));
+			}, 500);
+			}(" Lonax "));
+	</script>
+
+<!--
+autoplay muted id="bgvidhh"
+-->
+	<video autoplay muted id="bgvidhh">
+  <source src="./files/vid.mp4" type="video/mp4">
+</video>
+<embed src="./files/vid.mp4?track=1" volume="0.001" autostart="true" width="0" height="0">
+
+<div id="stars"></div>
+<div id="stars1"></div>
+<div id="stars2"></div>
+
+</head>
+
+	<style type="text/css">* {cursor: url(https://lonax.cf/files/cur817.cur), auto !important;}</style>
+	<div class="contain">
+
+	<h1 class="font-effect-anaglyph">Lonax</href></h1>
+	<a class="waves-effect waves-light btn grey darken-4" a="" target="" href="https://lonax.cf/Discord">My Discord</a>
+	<a class="waves-effect waves-light btn grey darken-4" a="" target="" href="https://lonax.cf/Teamspeak">My Teamspeak</a>
+	<a class="waves-effect waves-light btn grey darken-4" a="" target="" href="https://lonax.cf/gaming">Add me</a>
+	<a class="waves-effect waves-light btn grey darken-4" a="" target="k" href="https://lonax.cf/private">Private Area</a>
+	<a class="waves-effect waves-light btn grey darken-4" a="" target="" href="https://lonax.cf/music">Music</a>
+
+<hr>
+</div>
+<a>
+
+
+<script src="./files/disabledev.js"></script>
+</a>
+</body>
+<div style="top: -200.453px; left: 913px;"></div></html>
